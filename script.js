1a182ef3de6fe7c8e1a07a0c8cc40aa85d4c4bc9
Mayen Akpan <neroxma@yahoo.com>
2022-01-26 21:22:35 -0800

    updated

--- script.js
+++ script.js
@@ -12,6 +12,20 @@ function imageMode(color) {
   image2.src = `img/undraw_feeling_proud_${color}.svg`;
   image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
+
+function toggleDarkLightMode(isLight) {
+  nav.style.backgroundColor =  isLight ? "rgb(0 0 0 / 50%)" || "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+  imageMode("dark");
+
+  nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  imageMode("light");
+}
 // Dark Mode Styles
 function darkMode() {
   nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
